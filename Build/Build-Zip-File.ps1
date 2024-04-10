9816419ecacb729a0a76a6d40c6922827af1fddf
WillyMoselhy <20515747+WillyMoselhy@users.noreply.github.com>
2024-04-10 17:18:24 +0200

    Update build script

--- Build/Build-Zip-File.ps1
+++ Build/Build-Zip-File.ps1
@@ -31,5 +31,5 @@ Compress-Archive -Path .\FunctionApp\* -DestinationPath $folder\FunctionApp.zip 
 
 
 # Create json files for deployment
-bicep.exe build .\StandardSessionHostTemplate\DeploySessionHosts.bicep --outfile .\StandardSessionHostTemplate\DeploySessionHosts.json
-bicep.exe build .\deploy\bicep\DeployAVDSessionHostReplacer.bicep --outfile .\deploy\arm\DeployAVDSessionHostReplacer.json
+bicep build .\StandardSessionHostTemplate\DeploySessionHosts.bicep --outfile .\StandardSessionHostTemplate\DeploySessionHosts.json
+bicep build .\deploy\bicep\DeployAVDSessionHostReplacer.bicep --outfile .\deploy\arm\DeployAVDSessionHostReplacer.json

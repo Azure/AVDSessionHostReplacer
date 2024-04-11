f003e073c527741fa6d4cf1fa1b911f8e7c79432
WillyMoselhy <20515747+WillyMoselhy@users.noreply.github.com>
2024-04-11 10:58:22 +0200

    update output

--- Build/Build-Release-Body.ps1
+++ Build/Build-Release-Body.ps1
@@ -11,12 +11,14 @@ $urlDeployAVDSessionHostReplacer = "https://github.com/Azure/AVDSessionHostRepla
 $urlPortalUiUrl = "https://github.com/Azure/AVDSessionHostReplacer/releases/download/$Tag/portal-ui.json"  -replace ":", "%3A"  -replace "/", "%2F"
 
 $body = @"
+ReleaseBody<<EOF
 # Deploy This Release
 
 | Deployment Type           | Link                                                                                                                                                                                                                                                                                                                                                                                                                       |
 | :------------------------ | :------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
 | Azure Portal UI           | [![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#blade/Microsoft_Azure_CreateUIDef/CustomDeploymentBlade/uri/$urlDeployAVDSessionHostReplacer/uiFormDefinitionUri/$urlPortalUiUrl) |
 | Command line (Bicep/ARM)  | [![Powershell/Azure CLI](./docs/icons/powershell.png)](./docs/CodeDeploy.md)
+EOF
 "@
 
-echo "{ReleaseBody} = {$body}" >> $Env:GITHUB_OUTPUT
+$body >> $Env:GITHUB_OUTPUT

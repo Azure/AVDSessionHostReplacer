7f943dd429ea13bea3ecfe8a65847f15eb2c07ee
WillyMoselhy <20515747+WillyMoselhy@users.noreply.github.com>
2024-04-11 10:44:59 +0200

    Release Body

---
+++ Build/Build-Release-Body.ps1
@@ -0,0 +1,22 @@
+
+
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]
+    $Tag
+)
+
+$urlDeployAVDSessionHostReplacer = "https://github.com/Azure/AVDSessionHostReplacer/releases/download/$Tag/DeployAVDSessionHostReplacer.json" -replace ":", "%3A"  -replace "/", "%2F"
+$urlPortalUiUrl = "https://github.com/Azure/AVDSessionHostReplacer/releases/download/$Tag/portal-ui.json"  -replace ":", "%3A"  -replace "/", "%2F"
+
+$body = @"
+# Deploy This Release
+
+| Deployment Type           | Link                                                                                                                                                                                                                                                                                                                                                                                                                       |
+| :------------------------ | :------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| Azure Portal UI           | [![Deploy to Azure](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/#blade/Microsoft_Azure_CreateUIDef/CustomDeploymentBlade/uri/$urlDeployAVDSessionHostReplacer/uiFormDefinitionUri/$urlPortalUiUrl) |
+| Command line (Bicep/ARM)  | [![Powershell/Azure CLI](./docs/icons/powershell.png)](./docs/CodeDeploy.md)
+"@
+
+echo "{ReleaseBody} = {$body}" >> $Env:GITHUB_OUTPUT

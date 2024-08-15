ee405819e7c002a6cc7b802ab3d94f609bc2e635
GitHub Action <action@github.com>
2024-08-15 08:31:41 +0000

    Automated commit by GitHub Actions for new build [no ci]

--- FunctionApp/profile.ps1
+++ FunctionApp/profile.ps1
@@ -16,7 +16,7 @@ Set-PSFConfig -FullName PSFramework.Message.style.NoColor -Value $true #This is 
 
 ## Version Banner ## Updated by Build\Build-Zip-File.ps1
 
-Write-PSFMessage -Level Host -Message "This is SessionHostReplacer version {0}" -StringValues 'v0.2.9-beta.13'
+Write-PSFMessage -Level Host -Message "This is SessionHostReplacer version {0}" -StringValues 'v0.2.9-beta.14'
 
 
 # Import Function Parameters
